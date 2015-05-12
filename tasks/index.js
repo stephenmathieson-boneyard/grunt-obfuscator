38b99527e28c8074428d4e093dc5a12a9f5dd9af
alaimo <alaimojon@gmail.com>
2015-05-12 17:02:10 -0400

    support multi task, follow grunt config rules

--- tasks/index.js
+++ tasks/index.js
@@ -1,37 +1,30 @@
 
-var obfuscator = require('obfuscator');
-var fs = require('fs');
+var obfuscator = require('obfuscator'),
+  fs = require('fs');
 
 module.exports = function(grunt) {
-  grunt.registerTask('obfuscator', 'Your task description goes here.', function () {
-    var fn = this.async();
-    var entry = option('entry');
-    var root = option('root');
-    var out = option('out');
-    var strings = option('strings');
-    var files = option('files');
-
-    files = grunt.file.expand(files);
-    var options = new obfuscator.Options(files, root, entry, strings);
-
+  grunt.registerMultiTask('obfuscator', 'Obfuscate Node JS projects.', function () {
+    var fn = this.async(),
+      opts = this.options,
+      options = new Obfuscator.Options(
+        this.filesSrc,
+        opts.root,
+        opts.entry,
+        opts.strings
+      );
+      
     obfuscator(options, function (err, data) {
       if (err) {
-        grunt.log.error(err);
-        return fn(false);
+        return fn(err);
       }
 
-      fs.writeFile(out, data, function (err) {
+      fs.writeFile(opts.out, data, function (err) {
         if (err) {
-          grunt.log.error(err);
-          return fn(false);
+          return fn(err);
         }
 
         fn();
       });
     });
   });
-
-  function option(name) {
-    return grunt.config('obfuscator.' + name);
-  }
 };
