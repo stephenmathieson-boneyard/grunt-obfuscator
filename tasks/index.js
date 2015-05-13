9ee99d562597429fb298267c53755f4ca08d305c
alaimo <alaimojon@gmail.com>
2015-05-13 17:48:25 -0400

    allow overrides for uglify compressor options

--- tasks/index.js
+++ tasks/index.js
@@ -1,4 +1,3 @@
-
 var obfuscator = require('obfuscator'),
   fs = require('fs');
 
@@ -6,13 +5,21 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('obfuscator', 'Obfuscate Node JS projects.', function () {
     var fn = this.async(),
       opts = this.options(),
+      obfuscatorCompressDefaults = obfuscator.utils.compress.defaults,
+      obfuscatorCompressOpts = opts.compressor,
       options = new obfuscator.Options(
         this.filesSrc,
         opts.root,
         opts.entry,
         opts.strings
       );
-      
+
+    // allow overrides for uglify compressor options.
+    Object.keys(obfuscatorCompressDefaults).forEach(function(key) {
+      obfuscatorCompressOpts[key] = opts[key] || obfuscatorCompressDefaults[key];
+    });
+    options.compressor = obfuscatorCompressOpts;
+
     obfuscator(options, function (err, data) {
       if (err) {
         return fn(err);
